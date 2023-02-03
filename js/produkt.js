7265a8958fc81025b6761cf2669e578d87bba1a2
Caroline Madsen <caro3313@stud.kea.dk>
2023-02-03 12:51:30 +0100

    2

--- js/produkt.js
+++ js/produkt.js
@@ -1,1 +1,16 @@
 console.log("hej");
+
+const url = "https://kea-alt-del.dk/t7/api/products/1532";
+
+function hentData(){
+fetch(url)
+.then(res => res.json())
+.then(visProdukt);
+}
+
+function visProdukt(){
+    console.log(produkt);
+}
+
+
+hentData();
