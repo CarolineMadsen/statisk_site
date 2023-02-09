6021ebde686da02e15d63305ec10487db5dae112
Caroline Madsen <caro3313@stud.kea.dk>
2023-02-09 09:50:45 +0100

    9/2-23

--- js/produktliste.js
+++ js/produktliste.js
@@ -1,57 +1,47 @@
 console.log("Vi er i produktlisten");
 
-// https://kea-alt-del.dk/t7/api/products
+const urlParams = new URLSearchParams(window.location.search);
+const cat = urlParams.get("cat");
+const url = `https://kea-alt-del.dk/t7/api/products?limit=20&category=${cat}`;
 
+document.querySelector("h2").textContent = cat;
 
 // 1. Hente data:
-async function getData () {
-    const response = await fetch ("https://kea-alt-del.dk/t7/api/products?limit=10");
-    const data = await response.json();
-    // console.log(data);
+async function getData() {
+  const response = await fetch(url);
+  const data = await response.json();
+  // console.log(data);
 
-    // 2. loope igennem dataen // 3. For hver af dem
-    data.forEach (showProduct); 
-    };
-    function showProduct (product) {
-    console.log(product);
-        // 4. fange vores template
-        const template = document.querySelector("#smallproductTemplate").content;
-        const id = product.id;
-        const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
-        // console.log(template);
+  // 2. loope igennem dataen // 3. For hver af dem
+  data.forEach(showProduct);
+}
+function showProduct(product) {
+  console.log(product);
+  // 4. fange vores template
+  const template = document.querySelector("#smallproductTemplate").content;
+  const id = product.id;
+  const imagePath = `https://kea-alt-del.dk/t7/images/webp/640/${id}.webp`;
+  // console.log(template);
 
-        // 5. klone den
-        const copy = template.cloneNode(true);
-        // 6. skifte data
-            // 6.1. skiftning af tekst + billeder:
-            copy.querySelector("h3").textContent=product.productdisplayname;
-            copy.querySelector(".subtle").textContent=product.articletype;
-            copy.querySelector(".price").textContent=product.price;
-            copy.querySelector("a").href = "produkt.html?id="+product.id
-            copy.querySelector("img").src = imagePath;
-            
-            // 6.2 Hvordan man laver ting udsolgt
-            if(product.soldout){
-                copy.querySelector("article").classList.add("soldOut");
-            }
-            if(product.discount){
-                copy.querySelector("article").classList.add("onSale");
-            }
-        // 7. appende
-        document.querySelector("main").appendChild(copy);
-    }
-    // articletype:"Tshirts"
-    // brandname:"Nike"
-    // category:"Apparel"
-    // discount:null
-    // gender:"Men"
-    // id:1163
-    // price:895
-    // productdisplayname:"Sahara Team India Fanwear Round Neck Jersey"
-    // productionyear: 2011
-    // season: "Summer"
-    // soldout: 0
-    // subcategory: "Topwear"
-    // usagetype: "Sports"
+  // 5. klone den
+  const copy = template.cloneNode(true);
+  // 6. skifte data
+  // 6.1. skiftning af tekst + billeder:
+  copy.querySelector("h3").textContent = product.productdisplayname;
+  copy.querySelector(".subtle").textContent = product.articletype;
+  copy.querySelector(".price").textContent = product.price;
+  copy.querySelector("a").href = "produkt.html?id=" + product.id;
+  copy.querySelector("img").src = imagePath;
 
+  // 6.2 Hvordan man laver ting udsolgt
+  if (product.soldout) {
+    copy.querySelector("article").classList.add("soldOut");
+  }
+  if (product.discount) {
+    copy.querySelector("article").classList.add("onSale");
+    // copy.querySelector("ny_pris").textContent = Math.round(object.price - (object.price * object.discount) / 100);
+  }
+  // 7. appende
+  document.querySelector("main").appendChild(copy);
+}
 getData();
